30182242bec4149760365d55fc3a01f3f1190d89
MeechyFZ <ponsnicolas7@gmail.com>
2024-07-20 17:58:28 +0200

    Premier Commit

---
+++ JS/main.js
@@ -0,0 +1,60 @@
+
+
+// ============= TOGGLE ICON NAVBAR ============= //
+// ============= TOGGLE ICON NAVBAR ============= //
+
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('fa-xmark')
+    navbar.classList.toggle('active')
+}
+
+// ============= SCROLL SECTION ACTIVE LINK ============= //
+// ============= SCROLL SECTION ACTIVE LINK ============= //
+
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    sections.forEach( sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height) {
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*='+ id +']').classList.add('active');
+            });
+        };
+    });
+
+    // ============= STICKY NAVBAR ============= //
+    // ============= STICKY NAVBAR  ============= //
+    
+    let header = document.querySelector('header');
+    header.classList.toggle('sticky', window.scrollY > 100)
+    
+    // ============= REMOVE TOGGLE ICON AND NAVBAR ============= //
+    // ============= REMOVE TOGGLE ICON AND NAVBAR ============= //
+    
+    menuIcon.classList.remove('fa-xmark');
+    navbar.classList.remove('active');
+};
+
+// ============= SCROLL REVEAL ============= //
+// ============= SCROLL REVEAL ============= //
+
+ScrollReveal({ 
+    distance: '80px',
+    duration: 2000,
+    delay: 200,
+});
+
+ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
+ScrollReveal().reveal('.home-img, .skills-containers, .projects-box, .veille-content , .contact form', { origin: 'bottom'});
+ScrollReveal().reveal('.home-contact h1, .about-img', { origin: 'left' });
+ScrollReveal().reveal('.home-contact p, .about-content', { origin: 'right' });
